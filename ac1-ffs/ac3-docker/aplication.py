0b820b8aabb0177dfb7915c302e8dcad132cd61d
icaloooou <rockingrid6@gmail.com>
2022-05-16 22:30:42 -0300

    alter porta

--- ac1-ffs/ac3-docker/aplication.py
+++ ac1-ffs/ac3-docker/aplication.py
@@ -33,5 +33,5 @@ def listagem(nid):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5002))
+    port = int(os.environ.get("PORT", 5050))
     app.run(host='0.0.0.0', port=port)
